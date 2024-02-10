977428883db5c54b5fada7cfe1b1d23e5b2adad0
David <143638937+Cheessse@users.noreply.github.com>
2024-02-10 15:28:52 +0000

    completed homework

--- src/main.js
+++ src/main.js
@@ -1,21 +1,48 @@
 import iziToast from 'izitoast';
+import SimpleLightbox from 'simplelightbox';
 
 import 'izitoast/dist/css/iziToast.min.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const form = document.querySelector('.form');
 const gallery = document.querySelector('.gallery');
-const input = document.querySelector('.input');
-const container = document.querySelector('.container');
+const loader = document.querySelector('.loader');
 
-form.addEventListener('submit', e => {
+function showLoader() {
+  loader.classList.remove('hidden');
+}
+
+function hideLoader() {
+  loader.classList.add('hidden');
+}
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
   e.preventDefault();
   const q = e.target.elements.input.value;
   const key = '42276910-5dbc0617c597b0712888fd711';
 
-  findPhotos(key, q);
-
-  e.target.reset();
-});
+  if (q === '') {
+    return iziToast.error({
+      message:
+        'Sorry, there are no images matching your search query. Please try again!',
+      position: 'topRight',
+    });
+  } else {
+    showLoader();
+    findPhotos(key, q)
+      .then(data => {
+        createMarkup(data);
+        hideLoader();
+      })
+      .catch(error => {
+        console.error('Error fetching photos:', error);
+        hideLoader();
+      })
+      .finally(() => form.reset());
+  }
+}
 
 function findPhotos(key, q) {
   const BASE_URL = 'https://pixabay.com';
@@ -23,13 +50,35 @@ function findPhotos(key, q) {
   const PARAMS = `?key=${key}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true`;
   const url = BASE_URL + END_POINT + PARAMS;
 
-  return fetch(url)
-    .then(res => res.json())
-    .catch(error => {
-      iziToast.error({
-        message:
-          'Sorry, there are no images matching your search query. Please try again!',
-        position: 'topRight',
-      });
-    });
+  return fetch(url).then(res => res.json());
 }
+
+function createMarkup(data) {
+  const markup = data.hits
+    .map(data => {
+      return `<li class="gallery-item">
+             <a class="gallery-link" href="${data.webformatURL}">
+            <img class="gallery-image" src="${data.largeImageURL}" alt="${data.tags}" />
+            </a>
+            <p><b>Likes: </b>${data.likes}</p>
+            <p><b>Views: </b>${data.views}</p>
+            <p><b>Comments: </b>${data.comments}</p> 
+            <p><b>Downloads: </b>${data.downloads}</p>
+        </li>`;
+    })
+    .join('');
+
+  gallery.insertAdjacentHTML('afterbegin', markup);
+  const lightbox = new SimpleLightbox('.gallery a', options);
+  lightbox.refresh();
+  lightbox.on('show.simplelightbox');
+}
+
+const options = {
+  captions: true,
+  captionType: 'attr',
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+};
